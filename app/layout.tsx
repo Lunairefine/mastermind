c225e0aeeec9ff0c85cc00d9d1bce92891645566
Lunairefine <teamcyber2809@gmail.com>
2025-12-27 15:02:10 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,46 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Mastermind",
   description: "A simple yet challenging color puzzle game that tests logic, pattern recognition, and strategic thinking.",
+  metadataBase: new URL('https://lunairefine.vercel.app'),
+  openGraph: {
+    title: "Mastermind",
+    description: "A simple yet challenging color puzzle game that tests logic, pattern recognition, and strategic thinking.",
+    url: "https://lunairefine.vercel.app",
+    siteName: "Mastermind",
+    images: [
+      {
+        url: "/media/frame.png",
+        width: 1200,
+        height: 630,
+        alt: "Mastermind OG Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    title: "Mastermind",
+    description: "A simple yet challenging color puzzle game that tests logic, pattern recognition, and strategic thinking.",
+    card: "summary_large_image",
+    images: ["/media/frame.png"],
+  },
+  other: {
+    "fc:frame": JSON.stringify({
+    version: "next",
+    imageUrl: "https://lunairefine.vercel.app/media/frame.png",
+    button: {
+      title: "Play Logic Game",
+      action: {
+        type: "launch_frame",
+        name: "Mastermind webased",
+        url: "https://lunairefine.vercel.app",
+        splashImageUrl: "https://lunairefine.vercel.app/media/images/syntax.png",
+        splashBackgroundColor: "#000000",
+      },
+    },
+  }),
+    'base:app_id': '-',
+  },
 };
 
 export default function RootLayout({
